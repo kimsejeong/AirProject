db70691583111c87da567887b95ec01f7b084e10
kimsejeong <811252842@qq.com>
2020-12-15 23:09:38 +0800

    初始化前端代码

---
+++ front/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <label>用户名</label>
+    <div>
+      <input type="text" v-model.trim="name" placeholder="请输入标题" autocomplete="off" />
+    </div>
+
+    <label>密码</label>
+    <div>
+      <input type="password" v-model="password" placeholder="请输入密码" autocomplete="off" />
+    </div>
+
+    <label>验证码</label>
+    <div>
+      <input type="text" v-model="code" placeholder="请输入验证码" autocomplete="off" />
+    </div>
+
+    <button type="button" @click="checkForm">点击登陆</button>
+    <a>忘记密码</a>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ front/vue.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // 取消eslint报错
+  lintOnSave: false
+}
